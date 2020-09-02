d017675bc1b273caeef0256575b9abb92a29f09c
placid-tier-4550 <james.ardery@gmail.com>
2020-09-02 16:25:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,4 @@
 def position_taken?(board, index)
-  !(board[index] == " " || board[index] == "" || board[index] == nil)
+  # !(board[index] == " " || board[index] == "" || board[index] == nil)
+  board[index] == "O" || board[index] == "X" 
 end
